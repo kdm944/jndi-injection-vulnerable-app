c2831840813a536f0be1e3c7b96c6f9fa8c6c69f
kdm944 <sJyAndeKe1etGrO09lT1G6VNe>
2021-03-19 20:15:51 +0100

    Create lookup controller and prepare to deploy on weblogic

--- src/main/java/com/grzebielucha/poc/jndiinjection/JndiInjectionApplication.java
+++ src/main/java/com/grzebielucha/poc/jndiinjection/JndiInjectionApplication.java
@@ -2,9 +2,17 @@ package com.grzebielucha.poc.jndiinjection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class JndiInjectionApplication {
+public class JndiInjectionApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JndiInjectionApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JndiInjectionApplication.class, args);
--- src/main/java/com/grzebielucha/poc/jndiinjection/controller/LookupController.java
+++ src/main/java/com/grzebielucha/poc/jndiinjection/controller/LookupController.java
@@ -1,4 +1,4 @@
-package com.grzebielucha.poc.jndiinjection;
+package com.grzebielucha.poc.jndiinjection.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
